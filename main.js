8d87db48c056ca4855e3ed5895dae45243e84d81
geofs-pilot <geofspilot20@gmail.com>
2025-03-31 09:08:12 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,36 @@
 function toggleAircraftProperties() {
     let isOverpowered = false;
     let originalValues = { thrust: {}, zeroThrustAltitude: null, zeroRPMAltitude: null };
+    let lastAircraftID = geofs?.aircraft?.instance?.aircraftRecord?.id || null;
 
     function applyOverpoweredProperties() {
-        if (geofs && geofs.aircraft && geofs.aircraft.instance) {
+        if (geofs?.aircraft?.instance) {
             const aircraft = geofs.aircraft.instance;
 
-            if (originalValues.zeroThrustAltitude === null && aircraft.definition && aircraft.definition.zeroThrustAltitude !== undefined) {
+            if (originalValues.zeroThrustAltitude === null && aircraft.definition?.zeroThrustAltitude !== undefined) {
                 originalValues.zeroThrustAltitude = aircraft.definition.zeroThrustAltitude;
             }
 
-            if (originalValues.zeroRPMAltitude === null && aircraft.definition && aircraft.definition.zeroRPMAltitude !== undefined) {
+            if (originalValues.zeroRPMAltitude === null && aircraft.definition?.zeroRPMAltitude !== undefined) {
                 originalValues.zeroRPMAltitude = aircraft.definition.zeroRPMAltitude;
             }
 
             if (aircraft.definition) {
-                if (aircraft.definition.zeroThrustAltitude !== undefined) {
-                    aircraft.definition.zeroThrustAltitude = 300000;
-                }
-                if (aircraft.definition.zeroRPMAltitude !== undefined) {
-                    aircraft.definition.zeroRPMAltitude = 300000;
-                }
+                aircraft.definition.zeroThrustAltitude = 300000;
+                aircraft.definition.zeroRPMAltitude = 300000;
             }
 
             if (aircraft.parts) {
                 for (let partName in aircraft.parts) {
                     let part = aircraft.parts[partName];
-                    if (part && part.thrust !== undefined) {
+                    if (part?.thrust !== undefined) {
                         if (!originalValues.thrust[partName]) {
                             originalValues.thrust[partName] = {
                                 thrust: part.thrust,
                                 afterBurnerThrust: part.afterBurnerThrust || null
                             };
                         }
-
                         part.thrust = 900000;
-
                         if (part.afterBurnerThrust !== undefined) {
                             part.afterBurnerThrust = 900000;
                         }
@@ -46,23 +41,23 @@ function toggleAircraftProperties() {
     }
 
     function applyNormalProperties() {
-        if (geofs && geofs.aircraft && geofs.aircraft.instance) {
+        if (geofs?.aircraft?.instance) {
             const aircraft = geofs.aircraft.instance;
 
-            if (aircraft.definition && originalValues.zeroThrustAltitude !== null) {
-                aircraft.definition.zeroThrustAltitude = originalValues.zeroThrustAltitude;
-            }
-
-            if (aircraft.definition && originalValues.zeroRPMAltitude !== null) {
-                aircraft.definition.zeroRPMAltitude = originalValues.zeroRPMAltitude;
+            if (aircraft.definition) {
+                if (originalValues.zeroThrustAltitude !== null) {
+                    aircraft.definition.zeroThrustAltitude = originalValues.zeroThrustAltitude;
+                }
+                if (originalValues.zeroRPMAltitude !== null) {
+                    aircraft.definition.zeroRPMAltitude = originalValues.zeroRPMAltitude;
+                }
             }
 
             if (aircraft.parts) {
                 for (let partName in originalValues.thrust) {
                     let part = aircraft.parts[partName];
-                    if (part && part.thrust !== undefined) {
+                    if (part?.thrust !== undefined) {
                         part.thrust = originalValues.thrust[partName].thrust;
-
                         if (part.afterBurnerThrust !== undefined && originalValues.thrust[partName].afterBurnerThrust !== null) {
                             part.afterBurnerThrust = originalValues.thrust[partName].afterBurnerThrust;
                         }
@@ -93,6 +88,17 @@ function toggleAircraftProperties() {
     });
 
     console.log("Press 'Q' to toggle aircraft properties between normal and overpowered.");
+
+    // Monitor aircraft changes
+    setInterval(() => {
+        let currentAircraftID = geofs?.aircraft?.instance?.aircraftRecord?.id || null;
+        if (currentAircraftID !== lastAircraftID) {
+            console.log("Aircraft changed, resetting properties.");
+            originalValues = { thrust: {}, zeroThrustAltitude: null, zeroRPMAltitude: null };
+            isOverpowered = false;
+            lastAircraftID = currentAircraftID;
+        }
+    }, 500); // Check every 500ms
 }
 
 toggleAircraftProperties();
